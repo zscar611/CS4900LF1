cfbf06db52ad359b35fa8ba0914aa61df90cea73
timhealey3 <timhealey34@gmail.com>
2024-03-06 11:45:59 -0500

    testing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
+from flask_cors import CORS
 from website import create_app
 
 app = create_app()
-
+CORS(app)
 if __name__ == '__main__':
     app.run(debug=True)  # set to false at end of semester

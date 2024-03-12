667a55d5e741bc8a2e210d203eec3f29592c8387
timhealey3 <timhealey34@gmail.com>
2024-03-12 17:45:15 -0400

    hm

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from website import create_app
+from flask import Flask, request, jsonify
+from flaskcors import CORS
+
 
 app = create_app()
+CORS(app, origins='http://localhost:3000')
 if __name__ == '__main__':
     app.run(debug=True)  # set to false at end of semester

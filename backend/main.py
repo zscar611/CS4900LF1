9e89571e0be1b96b38ce9804636b426d59ec0468
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-03-13 00:55:05 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from website import create_app
 from flask import Flask, request, jsonify
-from flaskcors import CORS
-
+from flask_cors import CORS
 
 app = create_app()
 CORS(app, origins='http://localhost:3000')

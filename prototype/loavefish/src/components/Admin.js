168cc1e0aaebf3dc320faa0d8669a4a6534e005e
timhealey3 <timhealey34@gmail.com>
2024-03-14 16:36:18 -0400

    work on timeSheet

--- prototype/loavefish/src/components/Admin.js
+++ prototype/loavefish/src/components/Admin.js
@@ -1,28 +1,107 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './App.css';
-import './Admin.css';
-import Navbar from './Navbar.js';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import "./Admin.css";
+import Navbar from "./Navbar.js";
+
+// array of people schedulded for today but not checked in
+// array of people checked in
+// display all, display building/external, display area based
+// columns, name, location, time
+// check in or check out button
+// check out will ask are you sure
 
 function Admin() {
   const navigate = useNavigate();
-  const [currentUser, setCurrentUser] = useState("");
+  //const [checkIn, setCheckedIn] = useState([]);
+  //const [checkOut, setCheckOut] = useState([]);
+
+  const signInDict = [
+    {
+      name: "Lionel Messi",
+      area: "Pantry",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Kylian Mbappe",
+      area: "Warehouse",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Lionel Messi",
+      area: "Home Deliveries",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Luca Modric",
+      area: "Call Cener",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Lebron James",
+      area: "Pantry",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Kylian Mbappe",
+      area: "Home Deliveries",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Lionel Messi",
+      area: "Pantry",
+      timeScheduled: "10:30am - 11:30am",
+    },
+    {
+      name: "Kylian Mbappe",
+      area: "Warehouse",
+      timeScheduled: "10:30am - 11:30am",
+    },
+  ];
+
+  const signIn = (person) => {
+    console.log(person);
+  };
+
   const handleLogout = () => {
-      // TODO insert SQL logic here    
-      navigate('/');
+    // TODO insert SQL logic here
+    navigate("/");
   };
   return (
     <div className="App">
       <Navbar />
-      <div className = "admin-top-text">
-      <h1>Volunteer Time Sheet</h1>
-
+      <div className="admin-top-text">
+        <h1>Volunteer Time Sheet</h1>
       </div>
-      <p>Currently Checked In     -       Check In</p>
-      <p>Sauron | Pantry | 9:00am - 10:00am | 12/10/2023</p>
-      <p>Future Appointments</p>
-      <p>Frodo Baggins | Warehouse | 10:00am - 11:00am | 12/10/2023</p>
-      <p>Tom Brady | Warehouse | 13:00am - 11:30am | 12/10/2023</p>
+      <div className="row">
+        <div className="column">
+          <h2>Sign In</h2>
+          <ul>
+            {signInDict.map((person, index) => (
+              <div className="admin-person" key={index}>
+                <p>{person.name}</p>
+                <p>{person.area}</p>
+                <p>{person.timeScheduled}</p>
+                <button onClick={() => signIn(person)}>Sign In</button>{" "}
+              </div>
+            ))}
+          </ul>
+        </div>
+
+        <div className="column">
+          <h2>Sign Out</h2>
+          <ul>
+            {signInDict.map((person, index) => (
+              <div className="admin-person" key={index}>
+                <p>{person.name}</p>
+                <p>{person.area}</p>
+                <p>{person.timeScheduled}</p>
+                <button>Sign Out</button>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

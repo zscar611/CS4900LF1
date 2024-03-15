20eacfd29087ff329f094aa8c883b7442a5880ca
timhealey3 <timhealey34@gmail.com>
2024-03-15 17:30:18 -0400

    small fixes

--- prototype/loavefish/src/components/AdminHome.js
+++ prototype/loavefish/src/components/AdminHome.js
@@ -18,30 +18,29 @@ function AdminHome() {
       <div className="row">
         <div className="column">
           <div className="adminHome-box">
-            <h2>
+            <h2 >
               Today: {today.getMonth()} / {today.getDate()} /{" "}
               {today.getFullYear()}
             </h2>
-            <p>Current Volunteers: 10</p>
-            <p>Current in Building: 7</p>
-            <p>Current Outside: 3</p>
-            <p>Todays Volunteers: 45</p>
+            <p className="adminHome-text">Current Volunteers: 10</p>
+            <p className="adminHome-text">Current in Building: 7</p>
+            <p className="adminHome-text">Current Outside: 3</p>
           </div>
         </div>
         <div className="column">
           <div className="adminHome-box">
             <h2>Warehouse</h2>
-            <p>Current Volunteers: 3</p>
-            <p>Todays Volunteers: 5</p>
-            <p>Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
+            <p className="adminHome-text">Current Volunteers: 3</p>
+            <p className="adminHome-text">Todays Volunteers: 5</p>
+            <p className="adminHome-text">Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
           </div>
         </div>
         <div className="column">
           <div className="adminHome-box">
             <h2>Pantry</h2>
-            <p>Current Volunteers: 1</p>
-            <p>Todays Volunteers: 9</p>
-            <p>Checked In: Carlos Sainz, George Russell, Alex Albon</p>
+            <p className="adminHome-text">Current Volunteers: 1</p>
+            <p className="adminHome-text">Todays Volunteers: 9</p>
+            <p className="adminHome-text">Checked In: Carlos Sainz, George Russell, Alex Albon</p>
           </div>
         </div>
       </div>
@@ -49,25 +48,25 @@ function AdminHome() {
         <div className="column">
           <div className="adminHome-box">
             <h2>Call Center</h2>
-            <p>Current Volunteers: 4</p>
-            <p>Current in Building: 6</p>
-            <p>Checked In: Yuki Tsunoda, Daniel Ricciardo, Pierre Gasly</p>
+            <p className="adminHome-text">Current Volunteers: 4</p>
+            <p className="adminHome-text">Current in Building: 6</p>
+            <p className="adminHome-text">Checked In: Yuki Tsunoda, Daniel Ricciardo, Pierre Gasly</p>
           </div>
         </div>
         <div className="column">
           <div className="adminHome-box">
             <h2>Home Delivery</h2>
-            <p>Current Volunteers: 2</p>
-            <p>Todays Volunteers: 3</p>
-            <p>Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
+            <p className="adminHome-text">Current Volunteers: 2</p>
+            <p className="adminHome-text">Todays Volunteers: 3</p>
+            <p className="adminHome-text">Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
           </div>
         </div>
         <div className="column">
           <div className="adminHome-box">
             <h2>Front Desk</h2>
-            <p>Current Volunteers: 1</p>
-            <p>Todays Volunteers: 3</p>
-            <p>Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
+            <p className="adminHome-text">Current Volunteers: 1</p>
+            <p className="adminHome-text">Todays Volunteers: 3</p>
+            <p className="adminHome-text">Checked In: Charles Leclerc, Max Verstappen, Lewis Hamilton</p>
           </div>
         </div>
       </div>

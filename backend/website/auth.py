326ffb23bd9ae019b38c18cde03cc69be087148e
Ryan Frederick <rrn9475@wmich.edu>
2024-03-21 15:50:37 -0400

    auto generates password, asks for dob, checks for duplicates

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -31,29 +31,41 @@ def sign_up():
         phone_number = request.form.get('phone_number')
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+        date_of_birth = request.form.get('date_of_birth')
 
+        # auto generate password
+        password = last_name.lower()[:3] + first_name.lower()[:3] + date_of_birth[:2] + date_of_birth[6:]
+
+        # check for duplicates
+        user = User.query.filter(
+        User.first_name.like(first_name),
+        User.last_name.like(last_name),
+        User.phone_number.like(phone_number)
+        ).first()
+
+        if user:
+            message = {"ERROR": "User already exists"}
+            return jsonify(message), 400
+        
+        # error checking
         if len(phone_number) != 10:
             message = {"ERROR": "Invalid phone number."}
-            return jsonify(message)
+            return jsonify(message), 400
         elif len(first_name) < 2:
             message = {"ERROR": "First name must be greater than 2 characters."}
-            return jsonify(message)
+            return jsonify(message), 400
         elif len(last_name) < 2:
             message = {"ERROR": "Last name must be greater than 2 characters."}
-            return jsonify(message)
-        elif len(password1) < 12:
-            message = {"ERROR": "Password must be over 12 characters."}
-            return jsonify(message)
-        elif password1 != password2:
-            message = {"ERROR": "Passwords do not match."}
-            return jsonify(message)
+            return jsonify(message), 400
+        elif len(date_of_birth) != 10:
+            message = {"ERROR": "Invalid date of birth"}
+            return jsonify(message), 400
         else:
-            new_user = User(first_name=first_name, last_name=last_name, phone_number=phone_number, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(first_name=first_name, last_name=last_name, phone_number=phone_number, password=generate_password_hash(password, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
             message = {"SUCESS": "Account created."}
-            return jsonify(message)
+            return jsonify(message), 201 
     elif request.method == 'GET':
-        return jsonify({"message": "hi"}), 200
+        message = {"FORMAT": "first_name = string over 2 chars, last_name = string over 2 chars, phone_number = string of 10 chars, date_of_birth = string of 10 chars"}
+        return jsonify(message), 200

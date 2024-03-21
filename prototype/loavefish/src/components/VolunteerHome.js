c3f62a6f88eee372194b5e58acc821a0b1fbf49c
Zack Scarpelli <zscar611@gmail.com>
2024-03-21 14:40:44 -0400

    working on calendar still

--- prototype/loavefish/src/components/VolunteerHome.js
+++ prototype/loavefish/src/components/VolunteerHome.js
@@ -4,17 +4,29 @@ import './App.css';
 import Calendar from 'react-calendar';
 import './Calendar.css';
 import { differenceInCalendarDays } from 'date-fns';
-
+import { differenceInCalendarMonths } from 'date-fns';
 function isSameDay(a, b) {
   return differenceInCalendarDays(a, b) === 0;
 }
 
+function popup(props) 
+{
+  return <h1>Test!</h1>;
+}
+var active_index = 4000;
+var new_index = 4001;
 function VolunteerHome() {
-	
+	function getShiftData()
+	{
+		//TODO: get shift data from here
+	}
 	//CALENDAR STUFF
 	
-	//TODO: get json data for scheduled shift dates for a user here, add 1 to the day value for some weird reason to get it right ( e.g. april 3rd will highlight april 2nd)
-	const datesToAddClassTo = ['2024-03-08','2024-03-10','2024-03-15'];
+	//TODO: get json data for scheduled shift dates for a user here , (have 2 0s before number to work properly, 007, 0010, etc.) dates must be in chronological order!!!
+	const scheduledDays = [];
+	
+	//TODO: populate here with dates from scheduledDays for the selected month
+	const datesToAddClassTo = ['2024-03-007','2024-03-0010','2024-03-0015','2024-03-0021'];
 	
 	//applies styling to given days
 	function tileClassName({ date, view }) 
@@ -43,23 +55,67 @@ function VolunteerHome() {
 			// Check if a date React-Calendar wants to check is on the list of dates to add class to
 			if (datesToAddClassTo.find(dDate => isSameDay(dDate, date))) 
 			{
-			return <div class="popup">
+			return <div className="popup">
 			
-					<span class="popuptext" id="myPopup">helloooooooooooo!!!...</span>
-					<span class="dot"></span>
+					
+					<span className="popuptext">
+					hello
+					</span>
+					<span className="dot"></span>
 				   </div>;
 			}
 		}
 	}
 	
-	//WIP: show popup of shift information
+	//show popup of shift information
 	function showPopup(date) 
 	{
+		var popups = document.getElementsByClassName("popuptext");
+		//if active day is a scheduled day
 		if (datesToAddClassTo.find(dDate => isSameDay(dDate, date))) 
 			{
-				var popup = document.getElementById("myPopup");
-				popup.classList.toggle("show");
+				
+				//get index in list of scheduled days to show popup correctly
+				new_index = datesToAddClassTo.findIndex(dDate => isSameDay(dDate, date));
+				
+				//date is the same as before, don't toggle
+				if (new_index == active_index)
+				{
+					var test2 = 1;
+				}
+				else
+				{	
+					if (active_index != 4000)
+					{	
+						//toggles off old selected date
+						popups[active_index].classList.toggle("show");
+					}
+				
+					//shows popup over active index
+					active_index = new_index;
+					popups[active_index].classList.toggle("show");
+					
+					
+				}
 			}
+		else
+		{
+			console.log(active_index);
+			//if no day was previously selected, do nothing
+			if (active_index == 4000)
+			{
+					console.log(active_index);
+					var test = 0;
+			}
+			//if a day was previously selected, toggle it off
+			else
+			{
+			console.log(active_index);
+			popups[active_index].classList.toggle("show");
+			active_index = 4000;
+			
+			}
+		}
 	}
 	
 	
@@ -89,6 +145,8 @@ function VolunteerHome() {
         <img src={'/logo-burned.png'} className="main-img" alt="logo"/>
         <p className='dashboard-text'>Hello, Frodo!</p>
 		<p>Next Appointment: 12/10/2023 | 10:00am - 11:00am | Warehouse </p>
+		<p>Favorite Areas: Warehouse, Front Desk, Mailing</p>
+        <p>Total Hours Volunteered: 30 </p>
       </header>
 	 
 	 <center>
@@ -100,10 +158,10 @@ function VolunteerHome() {
 	  tileContent={tileContent}
       />
 	 </center>
-
+		
+		
+	
    
-      <p>Favorite Areas: Warehouse, Front Desk, Mailing</p>
-      <p>Total Hours Volunteered: 30 </p>
 	   <div className="main-button">
         <button className="main-button-box" onClick={handleLogout}>Logout</button>
       </div>

3781fbb83ee248da48e382d05e67a78d69da895b
Ryan Frederick <rrn9475@wmich.edu>
2024-03-22 13:36:19 -0400

    added search for user

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -15,13 +15,52 @@ def all():
         all_users.append({"first name": user.first_name, "last name": user.last_name})
     return json.dumps(all_users)
 
+# get users basic info
+@auth.route('/user', methods=['GET', 'POST'])
+def get_user():
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+
+        def serialize_user(user):
+            user_to_return = {}
+            user_to_return['first_name'] = user.first_name
+            user_to_return['last_name'] = user.last_name
+            user_to_return['phone_number'] = user.phone_number
+            user_to_return['date_of_birth'] = user.date_of_birth
+
+            return user_to_return
+        
+        return_list = []
+
+        users = User.query.filter(
+            User.first_name.like(first_name),
+            User.last_name.like(last_name),
+        ).all()
+
+        for user in users:
+            return_list.append(serialize_user(user))
+
+
+        if users:
+            return json.dumps(return_list), 200
+
+
+
+
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         phone_number = request.form.get('phone_number')
         password = request.form.get('password')
 
-        user = User.query.filter_by(phone_number=phone_number).first()
+        user = User.query.filter(
+        User.phone_number.like(phone_number),
+        User.password.like(password)
+        ).first()
+
         if user:
             if check_password_hash(user.password, password):
                 message = {"SUCESS": "User sucessfully logged in"}
@@ -41,7 +80,7 @@ def logout():
     pass
 
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/sign-up/', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         phone_number = request.form.get('phone_number')

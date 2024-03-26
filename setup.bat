cb65dea537b5916c15316b14bf6f59b8a3992156
FSawy <113072089+FSawy@users.noreply.github.com>
2024-03-26 14:34:52 -0400

    Updated bat file for setup with the new npm installs

--- setup.bat
+++ setup.bat
@@ -1,6 +1,11 @@
 cd ./prototype/loavefish
 npm install react-scripts
 npm install react-calendar
+npm install --save @fortawesome/fontawesome-svg-core
+npm install --save @fortawesome/free-solid-svg-icons
+npm install --save @fortawesome/free-regular-svg-icons
+npm install --save @fortawesome/free-brands-svg-icons
+npm install --save @fortawesome/react-fontawesome@latest
 cd ../../
 cd ./backend/.venv
 Scripts/activate
